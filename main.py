1c51db2744859fc0bb6480ed2ef212e91867ba73
Cortrax <aidenrphotos@gmail.com>
2023-02-17 09:27:04 -0500

    Add text box

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ root = Tk()
 root.title('Notepad')
 root.geometry('500x500')
 
+# Create text area
+textBox = Text(root, height=50, width=100, wrap=WORD)
+textBox.place(x=0, y=26)
+
 def save_file():
-    f = asksaveasfile(initialfile='Untitled.txt',
+    open_file = asksaveasfile(initialfile='Untitled.txt',
                       defaultextension='.txt', filetypes=[('All Files', '*.*'), ('Text Documents', '*.txt')])
+    if open_file is None:
+        return
+    text = str(textBox.get(1.0, END))
+    open_file.write(text)
+    open_file.close()
     
 def open_file():
     file = askopenfile(mode = 'r', filetypes=[('All Files', '*.*')])
+    if file is not None:
+        content = file.read()
+    textBox.insert(INSERT, content)
 # Save file button
 saveBtn = Button(root, text='Save File', command=lambda:save_file())
 saveBtn.place(x=0, y=0)
@@ -21,7 +33,3 @@ saveBtn.place(x=0, y=0)
 openBtn = Button(root, text='Open File', command=lambda:open_file())
 openBtn.place(x=55, y=0)
 root.mainloop()
-
-# Create text area
-textBox = Entry(root, width=100)
-textBox.place(x=10, y=20)
